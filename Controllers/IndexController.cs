362be6a1a0e26219daf23c2f46525188c427f681
unknown <nmoccia@S20770.sogeti.com>
2014-06-22 21:39:53 -0400

    Initial commit

---
+++ Controllers/IndexController.cs
@@ -0,0 +1,46 @@
+﻿using MVCSandbox.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MVCSandbox.Controllers
+{
+    public class IndexController : Controller
+    {
+        //
+        // GET: /Index/
+
+        public ActionResult Index ()
+        {
+            // Create the list
+            List<IndexModel> newList = new List<IndexModel>();
+
+            // Create the first item within the list
+            IndexModel newIndexModel = new IndexModel
+            {
+                ID = 1,
+                Description = "First Item",
+                Comments = "This is the first instance of the model"
+            };
+
+            // Create the second item within the list
+            IndexModel newIndexModel2 = new IndexModel
+            {
+                ID = 2,
+                Description = "Second Item",
+                Comments = "This is the second instance of the model"
+            };
+
+            // Add the items to the list
+            newList.Add(newIndexModel);
+            newList.Add(newIndexModel2);
+
+            // Send the model to the view
+            return View(newList);
+        }
+
+        public ActionResult Edit ()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/IndexModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCSandbox.Models
+{
+    public class IndexModel
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public string Comments { get; set; }
+    }
+}
